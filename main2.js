8457bea8f098e2381b0bdd32920c97e9b8b183b6
Bruno Cocco <Brunoe.cocco@gmail.com>
2024-07-01 19:30:12 +0300

    agregando funcion

--- main2.js
+++ main2.js
@@ -1,14 +1,21 @@
 const boton = document.getElementById("boton")
+const nuevoDiv = document.getElementsByClassName("respuesta")
 
 const pregunta = () =>{
     let dato = parseFloat(prompt("dame un numero por favor"))
     if (!isNaN(dato)){
         console.log("numero correcto")
+        cuenta(dato)
     }
     else{
         console.log("numero incorrecto")
     }
 }
 
-boton.addEventListener("click", pregunta)
+function cuenta(dato){
+    for (i=dato; i<=100; i++){
+        console.log(i)
+    }
+}
 
+boton.addEventListener("click", pregunta)
